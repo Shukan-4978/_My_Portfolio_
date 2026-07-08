b713679a5fc20906341b91681781c437e9458372
Shukan Prajapati <shukanprajapati29@gmail.com>
2026-07-08 16:30:55 +0530

    fix: mongoose Mixed type definition

--- server/src/models/PortfolioData.ts
+++ server/src/models/PortfolioData.ts
@@ -2,7 +2,7 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IPortfolioData extends Document {
   key: string;
-  value: mongoose.Types.Mixed;
+  value: any;
   updatedAt: Date;
 }
 
