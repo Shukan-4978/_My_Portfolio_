6c71279aba1dd7fdd308eb9b0576edb5ecd0ab3e
Shukan Prajapati <shukanprajapati29@gmail.com>
2026-09-25 11:52:42 +0530

    refactor(server): convert backend completely from TypeScript to pure JavaScript (.js)

--- server/dist/middleware/upload.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const uploadImage: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
-export declare const uploadMultiple: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
-//# sourceMappingURL=upload.d.ts.map
